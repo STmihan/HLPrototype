6a1eba266ea6e05c7e14e8510e225b4a585217e6
STmihan <mih.dunaev@gmail.com>
2022-04-10 00:30:39 +0300

    Implement animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,23 +5,58 @@ namespace Player
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
+        private static readonly int Velocity = Animator.StringToHash("Velocity");
+        
         [SerializeField] private float _speed;
+        [SerializeField] private Animator _animator;
 
         private Camera _camera;
         private PlayerInputs _input;
         private Rigidbody _rigidbody;
 
+        private bool _isAiming;
+        
+        private void OnEnable() => _input.Enable();
+        private void OnDisable() => _input.Disable();
+        
         private void Awake()
         {
             _input = new PlayerInputs();
             _camera = Camera.allCameras[0];
             _rigidbody = GetComponent<Rigidbody>();
+            _input.Player.Fire1.performed += _ => _isAiming = true;
+            _input.Player.Fire1.canceled += _ => _isAiming = false;
         }
 
         private void FixedUpdate()
         {
             Vector2 input = _input.Player.Movement.ReadValue<Vector2>();
 
+            if (_isAiming)
+            {
+                input = Vector2.zero;
+                AimState();
+            }
+            else MoveState(input);
+            
+            AnimationMove(input.normalized.magnitude);
+        }
+
+        private void AimState()
+        {
+            Vector2 mousePos = _input.Player.MousePosition.ReadValue<Vector2>();
+            Ray ray = _camera.ScreenPointToRay(mousePos);
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            if(plane.Raycast(ray, out float d))
+            {
+                Vector3 point = ray.GetPoint(d);
+                // RotateInDirection(point, 95);
+                transform.LookAt(point, transform.up);
+            }   
+        }
+
+        private void MoveState(Vector2 input)
+        {
             if (input.magnitude > 0)
             {
                 Move(input);
@@ -42,12 +77,25 @@ namespace Player
         {
             Vector3 direction = _camera.transform.rotation * new Vector3(input.x, 0, input.y);
             direction.y = 0;
-            Quaternion targetAngle = Quaternion.LookRotation(direction);
-            Quaternion angle = Quaternion.RotateTowards(transform.rotation, targetAngle, 15f);
+            RotateInDirection(direction, 15);
+        }
+
+        private void RotateInDirection(Vector3 direction, float maxDegreesDelta)
+        {
+            Quaternion targetAngle = Quaternion.LookRotation(direction, transform.up);
+            Quaternion angle = Quaternion.RotateTowards(transform.rotation, targetAngle, maxDegreesDelta);
             transform.rotation = angle;
         }
 
-        private void OnEnable() => _input.Enable();
-        private void OnDisable() => _input.Disable();
+        private void AnimationMove(float input)
+        {
+            float velocity = _animator.GetFloat(Velocity);
+
+            if (input != 0) velocity += Time.deltaTime * 5;
+            else velocity -= Time.deltaTime * 5;
+            
+            velocity = Mathf.Clamp(velocity, 0, 1);
+            _animator.SetFloat(Velocity, velocity);
+        }
     }
 }
